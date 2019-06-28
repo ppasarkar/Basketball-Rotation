5f8e25324572752cd9d99d32d120691f15c5ed19
ppasarkar <ppasarkar@gmail.com>
2019-06-28 11:55:20 -0400

    made player class

---
+++ __main__.py
@@ -0,0 +1,17 @@
+import random
+
+class Player:
+    def __init__ (self, name):
+        self.name = name 
+        self.count
+
+    def get_name (self):
+        return self.name
+
+    def play_count (self):
+        self.count+=1
+
+    def get_count(self)
+        return self.play_count
+
+
